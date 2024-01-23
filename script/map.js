6664536aa73c05c946362a06f8de4ae560bc7956
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-01-23 17:59:39 +0200

    v3

--- script/map.js
+++ script/map.js
@@ -1,7 +1,8 @@
 
 ymaps.ready(function () {
+    
     var myMap = new ymaps.Map('map', {
-            center: [54.722820, 20.498674],
+            center: [54.711868, 20.521644],
             zoom: 18
         }, {
             searchControlProvider: 'yandex#search'
@@ -13,8 +14,8 @@ ymaps.ready(function () {
         ),
 
         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Наше местоположение',
-            balloonContent: 'Советский проспект, 7'
+            hintContent: 'тц Ванда',
+            balloonContent: '9 этаж, кабинет 905'
         }, {
             // Опции.
             // Необходимо указать данный тип макета.
@@ -29,4 +30,20 @@ ymaps.ready(function () {
         });
     myMap.geoObjects
         .add(myPlacemark)
+    
+        //масштабирование на CTRL
+        myMap.behaviors.disable(['scrollZoom']);
+        var body = document.getElementsByTagName('body')[0];
+        body.onkeydown = callbackDown;
+        body.onkeyup = callbackUp;
+        function callbackDown(e){
+            if(e.keyCode === 17){
+                myMap.behaviors.enable(['scrollZoom']);
+            }
+        }
+        function callbackUp(e){
+            if(e.keyCode === 17){
+                myMap.behaviors.disable(['scrollZoom']);
+            }
+        }
 });
